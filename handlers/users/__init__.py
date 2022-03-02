c5b9b221bb1ef5d0611f26c81cee0aa73ed1987b
IlyaMuralha <muralhakuzmenko@gmail.com>
2022-03-02 16:17:47 +0500

    fixed main template

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,3 @@
 from . import help
 from . import start
-from . import testing_sorage_memory
-from . import form
 from . import echo
--- states/user_form.py
+++
@@ -1,7 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-
-class UserForm(StatesGroup):
-    name = State()
-    email = State()
-    phone = State()
